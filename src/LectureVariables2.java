12c64bbf533aafc9a81828b6abd601ca39519087
jessehartloff <hartloff@buffalo.edu>
2017-02-02 22:49:06 -0500

    initial commit

---
+++ src/LectureVariables2.java
@@ -0,0 +1,70 @@
+public class LectureVariables2 {
+
+
+
+    // 2 points
+    static int Q1() {
+        int first = 60;
+        int second = 4;
+
+        // TODO: Edit the following code to set the value of result to the addition of first and second
+        // Do not change the values stored in first or second
+        int result = 0;
+
+
+        return result;
+    }
+
+
+
+    // 2 points
+    static double Q2() {
+        double first = 1.68;
+        double second = 0.47;
+
+        // TODO: Edit the following code to set the value of result to the multiplication of first and second
+        // Do not change the values stored in first or second
+        double z = 0.0;
+
+
+        return z;
+    }
+
+
+
+
+    // 2 points
+    static void Q3() {
+        int container = 416;
+        // TODO: Print "container is storing the value " followed by the value of number on the same line.
+        // Note the space after the word value  (Do not change the value of container)
+
+    }
+
+
+
+
+    // 2 points
+    static String Q4() {
+        String first = "I understand";
+        String second = "String concatenation!";
+
+        // TODO: set result equal to the concatenation of first and second separated by a space
+        // Do not change the values of first or second
+        String result = "";
+
+
+        return result;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    	Q3();
+        // You can optionally use this main method to test your code without having to submit
+        // This main method will not be graded
+
+    }
+
+}
